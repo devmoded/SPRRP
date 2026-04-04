71f5bf8de470cba628d0bac95690386df0745783
devmoded <matveyykas@gmail.com>
2026-04-04 22:45:41 +0300

    feat: обновление сборки в соответствии с новыми возможностями Modpack Downloader

--- build_release.py
+++ build_release.py
@@ -3,9 +3,12 @@ from pathlib import Path
 
 DIST = Path('dist')
 FOLDERS = [Path('main'), Path('custom')]
+FILES = [Path('instructions.yml'), Path('mods_list.json')]
 
 print(f"Начало сборки '{DIST/'sprrp-pack.zip'}'")
 with zipfile.ZipFile(DIST / 'sprrp-pack.zip', 'w', compression=zipfile.ZIP_DEFLATED) as release:
+    for file in FILES:
+        release.write(file)
     for folder in FOLDERS:
         for file_path in folder.rglob('*'):
             if file_path.is_file():
