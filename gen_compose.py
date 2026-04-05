4a135ee1d5c21fa769cf646c62d5f71358f5eb17
devmoded <matveyykas@gmail.com>
2026-04-05 11:57:58 +0300

    feat: обновление и добавление некоторых скриптов для автоматизации

--- gen_compose.py
+++ gen_compose.py
@@ -1,9 +1,11 @@
+import json
 from pathlib import Path
 from string import Template
+from urllib.parse import urlparse, unquote
 
 # Mods
 PREFIX = "https://github.com/devmoded/SPRRP/raw/refs/heads/main"
-MODS_DIRS = [Path("main/mods"), Path("server/mods")]
+SERVER_MODS = Path("server/mods")
 EXCLUDED = [
     'fast-ip-ping-v1.0.8-mc1.21.1-fabric.jar',
     'lazy-language-loader-0.3.7.jar',
@@ -17,14 +19,43 @@ EXCLUDED = [
 COMPOSE_TEMPLATE = Path('compose.template.yml')
 COMPOSE = Path('compose.yml')
 
-# prepare mods list
-mods = []
+def main():
+    compose_mods = []
 
-for dir in MODS_DIRS:
-    for file in dir.iterdir():
-        if file.name not in EXCLUDED:
-            mods.append(f"{PREFIX}/{file}")
+    mods_list = _load_mods_list()
+    loader= mods_list.get('loader')
+    mc_version = mods_list.get('mc_version')
+    mods = mods_list.get('mods')
 
-t = Template(COMPOSE_TEMPLATE.read_text())
-# print(t.substitute(mods_list='\n        '.join(mods)))
-COMPOSE.write_text(t.substitute(mods_list='\n        '.join(mods)))
+    if loader is None:
+        raise RuntimeError('В \'mods_list.json\' не указано поле \'loader\'')
+    if mc_version is None:
+        raise RuntimeError('В \'mods_list.json\' не указано поле \'mc_version\'')
+    if mods is None:
+        raise RuntimeError('В \'mods_list.json\' не указано поле \'mods\'')
+
+    for mod in mods:
+        if isinstance(mod, dict) and isinstance(loader, str) and isinstance(mc_version, str):
+            mod_url = mod.get('url')
+            if mod_url is not None:
+                if mod_url.startswith('https://cdn.modrinth.com'):
+                    filename = unquote(urlparse(mod_url).path.split('/')[-1])
+                    if filename not in EXCLUDED:
+                        compose_mods.append(mod_url)
+                elif mod_url.startswith('local:'):
+                    filename = unquote(urlparse(mod_url.replace('local:', '')).path.split('/')[-1])
+                    if filename not in EXCLUDED:
+                        compose_mods.append(f"{PREFIX}/{filename}")
+
+    t = Template(COMPOSE_TEMPLATE.read_text())
+    # print(t.substitute(mods_list='\n        '.join(mods)))
+    COMPOSE.write_text(t.substitute(mods_list='\n        '.join(compose_mods)))
+
+def _load_mods_list() -> dict[str, str | list[dict[str, str]]]:
+    mods_list_path = Path('mods_list.json')
+    if not mods_list_path.exists():
+        raise FileNotFoundError(f"Список модов {mods_list_path} не существует")
+    return json.loads(mods_list_path.read_text())
+
+if __name__ == '__main__':
+    main()
---
+++ gen_mods_list.py
@@ -0,0 +1,15 @@
+import json
+
+from pathlib import Path
+
+from load_prism_mods_index import get_mods_urls
+
+MODS_LIST = Path('mods_list.json')
+
+mods_list = {
+    'loader': 'fabric',
+    'mc_version': '1.21.1',
+    'mods': get_mods_urls()
+}
+
+MODS_LIST.write_text(json.dumps(mods_list, indent=2))
